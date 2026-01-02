fab323e7c6f07a2039c214d7962648b7c758f189
adamdobrogowski <adam.dobro01@gmail.com>
2026-01-02 19:28:40 +0100

    Components setup

---
+++ memory-word-app/src/components/Countdown.jsx
@@ -0,0 +1,7 @@
+export default function Countdown() {
+  return (
+    <div>
+      <h1>Countdown</h1>
+    </div>
+  );
+}
---
+++ memory-word-app/src/components/StartTask.jsx
@@ -0,0 +1,7 @@
+export default function StartTask() {
+  return (
+    <div>
+      <h1>Start Task</h1>
+    </div>
+  );
+}
